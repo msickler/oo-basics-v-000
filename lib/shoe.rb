7fd27d18cb7dcfb6c0ec14c9006af57c45bbe8d4
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-29 19:39:18 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+   attr_reader :new
+
+   def initialize(new)
+     @new = new
+   end
+
+   def new
+     @new
+   end
+  
+end
